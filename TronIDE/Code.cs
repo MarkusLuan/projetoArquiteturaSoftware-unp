a484eeecaa066ace8755cddb4a5c4ec7e0146b98
Markus Luan <markus23_luan@hotmail.com>
2020-10-10 11:09:48 -0300

    Listando os diretórios

--- TronIDE/Code.cs
+++ TronIDE/Code.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,33 @@ namespace TronIDE
             InitializeComponent();
 
             txt_code.Text = modelo;
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(@"C:\Users\marku\Documents\projetos\Android");
+            if (directoryInfo.Exists)
+            {
+                BuildTree(directoryInfo, null);
+            }
+        }
+
+        private void BuildTree(DirectoryInfo dir, TreeNode node)
+        {
+            if (node == null)
+            {
+                node = treeView1.Nodes.Add(dir.Name);
+            } else
+            {
+                node = node.Nodes.Add(dir.Name);
+            }
+
+            foreach (DirectoryInfo subdir in dir.GetDirectories())
+            {
+                BuildTree(subdir, node);
+            }
+
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                node.Nodes.Add(file.Name);
+            }
         }
 
         private void Code_FormClosing(object sender, FormClosingEventArgs e)
